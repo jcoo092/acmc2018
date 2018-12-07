dffcd74e681b9fb71f273d417013bebab6b18752
James Cooper <jcoo092@UoA.auckland.ac.nz>
2018-12-07 18:46:38 +1300

    Modified the program to use a plain DU
    Where the colours no longer have an int associated with them.  This isn't needed, as they are always selected in order.
    
    Making this change has led to a significant speed-up in most cases.

--- Program.fs
+++ Program.fs
@@ -6,10 +6,16 @@ open System
 
 type IO = bool
 
-type Colour =
+(* type Colour =
     | Red of int
     | Green of int
-    | Blue of int
+    | Blue of int *)
+
+[<Struct>]
+type Colour =
+    | Red
+    | Green
+    | Blue
 
 type OutcomeMessage = IO * Colour list option
 
@@ -61,13 +67,13 @@ let r12n4 i maxSteps l1 =
 
 let r22im1 i (l2: L2) =
     if guardSExists l2 then // actually does nothing here really
-        [{l2 with C = Red i :: l2.C; Ai = i + 1;}; {l2 with Ai = 0; T = true}]
+        [{l2 with C = Red :: l2.C; Ai = i + 1;}; {l2 with Ai = 0; T = true}]
     else
         [l2]
 
 let r22i i n l2  =
     if l2.T && i < n then
-        [{l2 with C = Blue i :: l2.C; Ai = i + 1; T = false;}; {l2 with C = Green i :: l2.C; Ai = i + 1; T = false;}]
+        [{l2 with C = Blue :: l2.C; Ai = i + 1; T = false;}; {l2 with C = Green :: l2.C; Ai = i + 1; T = false;}]
     else
         [l2]
 
@@ -75,29 +81,34 @@ let r22Combo i n l2 = r22im1 i l2 |> List.collect (r22i i n)
 
 let r22nm1 n l2 =
     if l2.Ai = n && guardSExists l2 then //l2.s representing the guard
-        [{l2 with C = Red n :: l2.C; Ai = l2.Ai + 1; T = false;}; {l2 with T = true;}]
+        [{l2 with C = Red :: l2.C; Ai = l2.Ai + 1; T = false;}; {l2 with T = true;}]
     else
         [l2]
 
 let r22n n l2 =
     if l2.Ai = n then
-        [{l2 with T = false; C = Blue n :: l2.C; }; {l2 with T = false; C = Green n :: l2.C; }]
+        [{l2 with T = false; C = Blue :: l2.C; }; {l2 with T = false; C = Green :: l2.C; }]
     else
         [l2]
 
 let r22nCombo n l2 = r22nm1 n l2 |> List.collect (r22n n)
 
-let guardColours l2 =
+let guardColours k l2 =
     let checkSameColour edge =
         let i,j = edge
-        List.contains (Red i) l2.C && List.contains (Red j) l2.C ||
+        //printfn "i = %d, j = %d" i j
+        (* List.contains (Red i) l2.C && List.contains (Red j) l2.C ||
         List.contains (Blue i) l2.C && List.contains (Blue j) l2.C ||
-        List.contains (Green i) l2.C && List.contains (Green j) l2.C
+        List.contains (Green i) l2.C && List.contains (Green j) l2.C *)
+        if i <= k && j <= k then
+            (l2.C.[i - 1]) = (l2.C.[j - 1])
+        else
+            true
 
     Array.exists checkSameColour (l2.Aij)
 
-let r22np1 l2 =
-    if guardColours l2 then
+let r22np1 k l2 =
+    if guardColours k l2 then
         {l2 with s = false;}
     else
         l2
@@ -109,13 +120,13 @@ let main argv =
 
     let timer = System.Diagnostics.Stopwatch ()
 
-    let numNodes = 150
+    let numNodes = 12
 
-    let E = [|for i in 1..numNodes do
+    (* let E = [|for i in 1..numNodes do
                         for j in i..numNodes do
                             if i <> j then
                                 yield (i, j)
-                    |]
+                    |] *)
 
     (* let E = [|
         for i in 2..numNodes do
@@ -125,7 +136,7 @@ let main argv =
     timer.Start()
 
     //let E = [(1, 2); (1, 3); (1, 4);]
-    //let E = [|(1, 2); (2, 3); (3, 4); (3, 5); (3, 6); (4, 7); (5, 9); (6, 7); (6, 10); (7, 11); (8, 9); (9, 10); (9, 12);|]
+    let E = [|(1, 2); (2, 3); (3, 4); (3, 5); (3, 6); (4, 7); (5, 9); (6, 7); (6, 10); (7, 11); (8, 9); (9, 10); (9, 12);|]
     //let E = [(1, 2); (1, 3); (1, 4); (1, 5); (1, 6); (1, 7); (1, 8); (1, 9); (1, 10)]
     //let E = [|(1, 3); (1, 4); (1, 6); (2, 4); (2, 5); (2, 7); (3, 5); (3, 8); (4, 9); (5, 10); (6, 7); (6, 10); (7, 8); (8, 9); (9, 10)|] // Petersen graph
 
@@ -143,13 +154,13 @@ let main argv =
         match i with
         | Odd ->
             let j = (i + 1) / 2
-            C2 <- List.collect (r22im1 j) C2 |> List.map r22np1 |> List.filter (fun c -> c.s)
+            C2 <- List.collect (r22im1 j) C2 |> List.map (r22np1 j) |> List.filter (fun c -> c.s)
         | Even ->
             let j = i / 2
             C2 <- List.collect (r22i j numNodes) C2
 
     C2 <- List.collect (r22nCombo numNodes) C2
-    C2 <- List.map r22np1 C2 |> List.filter (fun c -> c.s)
+    C2 <- List.map (r22np1 numNodes) C2 |> List.filter (fun c -> c.s)
 
     //printfn "C1: %A" C1
     //List.iter (fun c -> printfn "%A" c) C2
@@ -173,7 +184,7 @@ let main argv =
     timer.Stop()
 
     match outcome with
-    | Some colours -> printfn "Colouring is possible!  A potential solution is:  %A" colours
+    | Some colours -> printfn "Colouring is possible!  A potential solution is:  %A" (List.zip [numNodes.. -1 ..1] colours)
     | None -> printfn "No colouring is possible!"
 
     printfn "Total time taken was %02fs" timer.Elapsed.TotalSeconds
